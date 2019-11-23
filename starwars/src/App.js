1a7de547b03eddc7640faff528f6dc4aea923086
Tony Walker <Walkertony509@gmail.com>
2019-11-22 20:12:11 -0800

    I have completed MVP and answered the questions.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import CharacterList from "./components/CharacterList";
+import PageSelector from './components/PageSelector';
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      starwarsCharacters: [],
+      next: null,
+      previous:null
+    };
+  }
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  componentDidMount() {
+    this.getCharacters('https://swapi.co/api/people/');
+  }
 
-  return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-    </div>
-  );
+  getCharacters = URL => {
+    fetch(URL)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({ 
+          starwarsCharacters: data.results,
+          next: data.next,
+          previous:data.previous
+        });
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  };
+
+getNextCharacters = () => {
+  this.getCharacters(this.state.next);
+};
+
+getPreviousCharacters = () => {
+  this.getCharacters(this.state.previous);
+};
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="Header">React Wars</h1>
+        <PageSelector
+          next={this.state.next}
+          previous={this.state.previous}
+          getNext={this.getNextCharacters}
+          getPrevious={this.getPreviousCharacters} 
+          />
+        <CharacterList character={this.state.starwarsCharacters} />
+      </div>
+    );
+  }
 }
 
 export default App;
